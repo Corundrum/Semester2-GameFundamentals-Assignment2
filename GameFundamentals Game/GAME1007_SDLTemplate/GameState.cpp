970a11d748ba5f21104d06c1678a93d42d11b5a3
Corundrum <Simon.Sereda@georgebrown.ca>
2022-04-07 21:33:48 -0400

    added the moving background to game state

--- GameFundamentals Game/GAME1007_SDLTemplate/GameState.cpp
+++ GameFundamentals Game/GAME1007_SDLTemplate/GameState.cpp
@@ -1,22 +1,67 @@
 #include "GameState.h"
 #include "Engine.h"
+#include "TextureManager.h"
 
 GameState::GameState() {}
 
 void GameState::Enter()
 {
 	std::cout << "entering Test Play State..." << std::endl;
+	m_pBackgroundSrcs.reserve(3);
+	m_pBackgroundDsts.reserve(2);
+	m_pMidgroundDsts.reserve(5);
+	m_pForegroundDsts.reserve(3);
 
+	m_pBackgroundSrcs.push_back(new SDL_Rect{ 0,0,1024,768 });
+	m_pBackgroundSrcs.push_back(new SDL_Rect{ 1024,0,256,512 });
+	m_pBackgroundSrcs.push_back(new SDL_Rect{ 1024,512,256,512 });
+
+	m_pBackgroundDsts.push_back(new SDL_Rect{ 0,0,1024,768 });
+	m_pBackgroundDsts.push_back(new SDL_Rect{ 1024,0,1024,768 });
+
+	m_pMidgroundDsts.push_back(new SDL_Rect{ 0,0,256,512 });
+	m_pMidgroundDsts.push_back(new SDL_Rect{ 256,0,256,512 });
+	m_pMidgroundDsts.push_back(new SDL_Rect{ 512,0,256,512 });
+	m_pMidgroundDsts.push_back(new SDL_Rect{ 768,0,256,512 });
+	m_pMidgroundDsts.push_back(new SDL_Rect{ 1024,0,256,512 });
+
+	m_pForegroundDsts.push_back(new SDL_Rect{ 0,512,512,256 });
+	m_pForegroundDsts.push_back(new SDL_Rect{ 512,512,512,256 });
+	m_pForegroundDsts.push_back(new SDL_Rect{ 1024,512,512,256 });
+
+	TEMA::Load("images/placeHolderBG.png", "background");
 }
 
 void GameState::Update()
 {
-
+	for (auto background : m_pBackgroundDsts)
+	{
+		background->x -= 4;
+	}
+	for (auto midground : m_pMidgroundDsts)
+	{
+		midground->x -= 5;
+	}
+	for (auto foreground : m_pForegroundDsts)
+	{
+		foreground->x -= 7;
+	}
 }
 
 void GameState::Render()
 {
-
+	for (auto background : m_pBackgroundDsts)
+	{
+		SDL_RenderCopy(Engine::Instance().GetRenderer(), TEMA::GetTexture("background"), m_pBackgroundSrcs[0], background);
+	}
+	for (auto midground : m_pMidgroundDsts)
+	{
+		SDL_RenderCopy(Engine::Instance().GetRenderer(), TEMA::GetTexture("background"), m_pBackgroundSrcs[1], midground);
+	}
+	for (auto foreground : m_pForegroundDsts)
+	{
+		SDL_RenderCopy(Engine::Instance().GetRenderer(), TEMA::GetTexture("background"), m_pBackgroundSrcs[2], foreground);
+	}
 }
 
 void GameState::Exit()
--- GameFundamentals Game/GAME1007_SDLTemplate/GameState.h
+++ GameFundamentals Game/GAME1007_SDLTemplate/GameState.h
@@ -2,11 +2,17 @@
 #ifndef __GAME_STATE__
 #define __GAME_STATE__
 
+#include <vector>
+
 #include "States.h"
 
 class GameState : public State
 {
 private:
+	std::vector<SDL_Rect*> m_pBackgroundSrcs;
+	std::vector<SDL_Rect*> m_pBackgroundDsts;
+	std::vector<SDL_Rect*> m_pMidgroundDsts;
+	std::vector<SDL_Rect*> m_pForegroundDsts;
 
 public:
 	GameState();
